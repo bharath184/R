ea17ac95105a3c5d93b148edf0795df48c2b9277
bharath184 <bharath.nn.23.beai@acharya.ac.in>
2024-09-18 10:58:21 +0530

    Add files via upload

---
+++ variables.R
@@ -0,0 +1,145 @@
+a=90
+b<-40
+30->c
+._a=90
+._=39
+ls()
+b<<-50
+b<<-5
+help(rm)
+._=50
+class(._)
+._
+a=50
+b=a
+b
+a=b=c
+c
+c=a+b
+c
+a=2*b
+a
+b
+.=50
+.
+.a=90
+._1<-9
+ls()
+is.integer(b)
+{
+x=as.integer(raedline("enter a value"))
+y=as.integer(readline("enter a value"))
+}
+x
+y
+x=readline("enter number")
+x
+x=as.integer(x)
+x
+{
+  x=as.integer(raedline("enter a value"))
+  y=as.integer(readline("enter a value"))
+}
+z=as.integer(readline("enetre nuber"))
+{
+  p=as.integer(readline("enter a value"))
+  q=as.integer(readline("enter a value"))
+  r=as.integer(readline("enetre nuber"))
+}
+p+q+r
+
+
+
+i=60
+print(i)
+print("hello","world")
+print("hello")
+paste("hello","everyone")
+paste("hello all!! my name is",readline("enter your name"))
+name=readline("enter the name")
+paste("hello all!! my name is",name)
+{
+name=readline("enter the name")
+sprintf("hello all!! my name is %s",name)
+}
+
+
+x="hello"
+y="world"
+cat(x,y,file=)
+
+
+
+
+a=5
+b=3
+c=a/b
+d=a%%b
+e=a%/%b
+
+
+
+x=12
+y=26
+cat(a==b,file="operator.txt")
+cat(a!=b,file="operator.txt")
+cat(a<=b,file="operator.txt")
+cat(a>=b,file="operator.txt")
+cat(a<b,file="operator.txt")
+cat(a>b,file="operator.txt")
+cat( a<b,"\n",a>b,"\n",a<=b,"\n",a>=b,"\n",a==b,"\n",a!=b,"\n",file="operator.txt")
+
+
+
+
+cat(a==b,file="operator.txt",append=TRUE,"\n")
+cat(a!=b,file="operator.txt",append=TRUE,"\n")
+cat(a<=b,file="operator.txt",append=TRUE,"\n")
+cat(a>=b,file="operator.txt",append=TRUE,"\n")
+cat(a<b,file="operator.txt",append=TRUE,"\n")
+cat(a>b,file="operator.txt",append=TRUE,"\n")
+
+x=readline("enter the first string")
+y=readline("enter the second string")
+cat(x==y)
+output=(x!=y)
+output
+cat(x<=y)
+
+
+
+#logical
+&=AND
+|=OR
+!=not
+
+
+
+
+
+("a"&"b")
+(10&20)
+(0&1)
+(7&-1)
+(-8&0)
+(0&0)
+(1|1)
+(7|0)
+(0|0)
+(0|40)
+
+
+cgpa=as.double(readline("enter the cgpa"))
+cer=readline("enter if certificate is done or not?(yes/no)")
+#case1
+eleg=((cgpa>8.0) && (cer=="yes"))
+eleg
+#case2
+eleg=((cgpa>8.0) || (cer=="yes"))
+eleg
+
+vec1[1]=3
+vec5[3]=8
+vec5
+
+
